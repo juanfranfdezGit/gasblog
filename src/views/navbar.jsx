410e2437803df090f668fad92078baf385ee453e
juanfranfdezGit <jfranromfdez@gmail.com>
2024-10-04 09:16:39 +0200

    logo out

--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -1,11 +1,9 @@
 import './navbar.css'
-import Logo from '../assets/logo.webp'
 
 function Navbar() {
     
     return (
         <nav className='flex navbar'>
-            <img src={Logo} alt="Gas blog"></img>
             <ul className='flex'>
                 <li><a href="#Novedades">Novedades</a></li>
                 <li><a href="#Blog">Blog</a></li>
